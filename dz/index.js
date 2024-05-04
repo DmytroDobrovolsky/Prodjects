f86e70ff7d1ceb5cbdbfecb1996e901bf1f9f3aa
Dmytro <olegterp7@gmail.com>
2024-05-05 01:08:49 +0300

    first task

---
+++ dz/index.js
@@ -0,0 +1,26 @@
+//let d = 0;
+//let getDrinks = 0;
+//while (d <= 5) {
+//d = d + 1;
+//console.log('d:',d);
+//getDrinks = getDrinks + d;
+//console.log("getDrinks:",getDrinks);
+//}
+
+// First task //
+let d = 0;
+let getDrinks = 0;
+
+do {
+  console.log('d:',d);
+getDrinks = getDrinks + d;
+
+d = d + 1;
+
+} while (d <= 10);
+console.log("getDrinks:",getDrinks);
+
+//console.log(getDrinks(5)); it`s: 15;
+//console.log(getDrinks(0)); it`s: 0;
+//console.log(getDrinks(6)); it`s: 21;
+//console.log(getDrinks(10)); it`s: 55;
