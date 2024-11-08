11a289615af77c2075945ed212470fa6ec6ea7e5
Dmytro Dobrovolsky <olegterp7@gmail.com>
2024-11-09 00:29:20 +0200

    task 5

--- Objects/index.js
+++ Objects/index.js
@@ -26,6 +26,8 @@ const students = [{
   }
 }];
 
+// Task 1 (як написати код, так аби саббжекс, який складається  з двох слів писав першу літеру в обох словах з великої букви)
+
  const getSubjects = (students) => {
 
 const studentKeys = Object.keys (students.subjects);
@@ -68,7 +70,7 @@ const studentsSubjects = getSubjects (students[0]);
 
 console.log(studentsSubjects);
 
-///////////
+// Task 2 
 
 const getAverageMark = (students) => {
 
@@ -85,6 +87,7 @@ const getAverageMark = (students) => {
 
   });
 
+  
 
   let sum = 0;
 
@@ -103,3 +106,200 @@ const getAverageMark = (students) => {
 const studentAverageMark = getAverageMark(students[0]);
 
 console.log(studentAverageMark);
+
+
+// Task 3
+
+const  getStudentInfo = (students) => {
+
+  const infoObject = {};
+
+  infoObject.name = students.name;
+
+  infoObject.course = students.course;
+
+  const subjKeys = Object.keys(students.subjects);
+
+  const marksArray = [];
+
+  subjKeys.forEach(key => {
+
+    marksArray.push(...students.subjects[key]);
+
+  });
+
+  
+
+  let sum = 0;
+
+  marksArray.forEach(mark => {
+
+    sum += mark;
+
+
+  });
+
+ let result = (sum / marksArray.length).toFixed(2);
+
+  infoObject.averageMark = + result;
+
+  console.log(infoObject);
+
+}
+
+
+const personalInfo = getStudentInfo(students[0]);
+
+// Task 4 (пояснити чому студенті має бути в лапках фор іч , бо обджектс не підійшло)
+
+
+const getStudentsNames = (students) => {
+
+const personalName = [];
+
+students.forEach(students => {
+
+personalName.push(students.name)
+});
+
+
+
+personalName.sort();
+
+return personalName;
+
+}
+
+const sortedName = getStudentsNames (students) ;
+
+console.log(sortedName);
+
+// Task 5 
+
+const getBestStudent = (students) =>{
+
+  const allAverageMark = [];
+
+  const subjKeysOne = Object.keys(students[0].subjects);  
+
+  const marksArrayOne = [];
+
+  subjKeysOne.forEach(key => {
+
+    marksArrayOne.push(...students[0].subjects[key]);
+
+  });
+
+  
+
+  let sumOne = 0;
+
+  marksArrayOne.forEach(mark => {
+
+    sumOne += mark;
+
+
+  });
+
+  let resultOne = (sumOne / marksArrayOne.length).toFixed(2);
+
+  allAverageMark.push(+resultOne);
+
+  console.log(resultOne);
+
+  
+  
+  const subjKeysTwo = Object.keys(students[1].subjects);  
+
+  const marksArrayTwo = [];
+
+  subjKeysTwo.forEach(key => {
+
+    marksArrayTwo.push(...students[1].subjects[key]);
+
+  });
+
+  
+
+  let sumTwo = 0;
+
+  marksArrayTwo.forEach(mark => {
+
+    sumTwo += mark;
+
+
+  });
+
+  let resultTwo = (sumTwo / marksArrayTwo.length).toFixed(2);
+
+  allAverageMark.push(+resultTwo);
+
+
+  console.log(resultTwo);
+  
+
+  
+  const subjKeysThree = Object.keys(students[2].subjects);  
+
+  const marksArrayThree = [];
+
+  subjKeysThree.forEach(key => {
+
+    marksArrayThree.push(...students[2].subjects[key]);
+
+  });
+
+  
+
+  let sumThree = 0;
+
+  marksArrayThree.forEach(mark => {
+
+    sumThree += mark;
+
+
+  });
+
+  let resultThree = (sumThree / marksArrayThree.length).toFixed(2);
+
+  allAverageMark.push(+resultThree);
+
+
+  console.log(resultThree);
+
+  console.log(allAverageMark);
+
+
+  const bestStudentOfAll = [];
+
+ allAverageMark.forEach(mark => {
+
+  if(allAverageMark[0]>allAverageMark[1]&&allAverageMark[0]>allAverageMark[2]){
+
+    bestStudentOfAll.push("Tanya");
+    
+  }
+
+  if(allAverageMark[1]>allAverageMark[0]&&allAverageMark[1]>allAverageMark[2]){
+
+    bestStudentOfAll.push("Victor");
+
+
+  }
+
+  else{
+
+    
+   bestStudentOfAll.push("Anton");
+    
+
+  }
+    
+  });
+
+return bestStudentOfAll;
+}
+
+const topStudent =  getBestStudent  (students);
+
+console.log(topStudent);
