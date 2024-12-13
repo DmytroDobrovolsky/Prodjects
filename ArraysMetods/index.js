eb60bb8e487b8d8c2f6d7c79413c3ba6927d94e7
Dmytro Dobrovolsky <olegterp7@gmail.com>
2024-12-13 22:41:42 +0200

    last changes

--- ArraysMetods/index.js
+++ ArraysMetods/index.js
@@ -8,7 +8,6 @@ function getRandomArray(length, min, max) {
 
         array.push(Math.floor(Math.random() * (max - min) + min));
 
-        console.log("task 1 :",array);
         
 
     }
@@ -17,37 +16,33 @@ function getRandomArray(length, min, max) {
 
 
 
-getRandomArray(10, 1, 50);
+const getRandArr = getRandomArray(10, 1, 50);
 
-// Завдання 2
+console.log("task 1:",getRandArr);
 
-// завдання 3
+// Завдання 3
 
 function getAverage(numbersTeam){
 
     let sum = 0;
 
-    for(let i = 0; i<=numbersTeam.length;i++){        
+    for(let i = 0; i<numbersTeam.length;i++){        
 
-        sum += i;
+        sum +=numbersTeam[i];
         
 
     }
     let average = sum / numbersTeam.length;
     
-    console.log("task 3 :",average);
-
     return average;
 }
 
  const numbersTeam = [10, 10, 10, 10, 10];
 
-getAverage(numbersTeam);
+const getAver = getAverage(numbersTeam);
 
+console.log('task 3:',getAver);
 
-
-
-//Завдання 4
 
 //Завдання 5
 
@@ -57,12 +52,11 @@ function filterEvenNumbers(numbersArray){
 
     for (let i = 0; i<=numbersArray.length;i++){
 
-        if(i%2==1){
+        if(numbersArray[i]%2==1){
 
 
-        oddNumbers.push(i);
+        oddNumbers.push(numbersArray[i]);
 
-        console.log("task 5 :",oddNumbers);
         
         }
 
@@ -73,7 +67,10 @@ function filterEvenNumbers(numbersArray){
 
 let numbersArray = [1, 2, 3, 4, 5,7,7,7,8,6];
 
-filterEvenNumbers(numbersArray);
+const filtEvnNum = filterEvenNumbers(numbersArray);
+
+console.log("task 5: ",filtEvnNum);
+
 
 //Завдання 6
 
@@ -89,12 +86,15 @@ let result = 0;
             
             result += 1;
 
-            console.log("task 6 :",result);
             
         }
 
     }
+
+ console.log("task 6 :",result);
+
 return result;
+
 }
 
 const numbersBlock = [-2, 3, -4, -5, 6];
@@ -140,3 +140,43 @@ const str = "Are you fucking kidding?";
 const res = replaceBadWords(str);
 
 console.log("task 8:",res);
+
+//Завдання 9
+
+const divideByThree = (_str) => {
+
+    const str = _str.toLowerCase();
+
+    const res = [];
+
+for(let i =0; i<str.length;i+=3){
+
+    let letters = "";
+
+if(str[i]){
+
+    letters += str[i];
+
+}
+
+if(str[i+1]){
+
+    letters+= str[i+1];
+}
+
+if(str[i+2]){
+
+    letters+=str[i+2];
+}
+
+res.push(letters);
+
+}
+
+return res;
+
+}
+
+const divideThree = divideByThree ("Commander");
+
+console.log("task 9:",divideThree);
